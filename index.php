9945d4b05597f1cfdb23022f48025f6cc545fbb7
Septo Distiano Rifky <itodistiano22@gmail.com>
2021-11-02 12:18:10 +0700

    menambahkan paragraf

---
+++ index.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>MDPL_Prak_6</title>
+</head>
+<body>
+    <h3>Materi Git Remote</h3>
+    <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ipsa sequi cupiditate dignissimos, repudiandae quisquam eum nobis veritatis aut libero cumque quia! Totam provident dolores voluptatem odit eos exercitationem maiores voluptatum.</p>
+</body>
+</html>
